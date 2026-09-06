2852d1ebd39676d81a852555e208abc307d8cc43
Payton <psides83@hotmail.com>
2026-09-06 14:28:51 -0500

    domian SEO

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "(?<host>.*\\.vercel\\.app)" }],
+        headers: [{ key: "X-Robots-Tag", value: "noindex" }]
+      }
+    ];
+  },
   images: {
     unoptimized: true,
     remotePatterns: [
